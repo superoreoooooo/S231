3e5f3f3992a9ed95714d7f403baed2a53fa04350
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-03-09 11:16:48 +0900

    Update 0309.py

--- 0309.py
+++ 0309.py
@@ -1,8 +1,10 @@
+"""
+
 def getkg(lb):
     return lb * 0.453592
 
 def getlb(kg):
-    return kg * 2.204623,
+    return kg * 2.204623
 
 
 lb = int(input("input lb : "))
@@ -12,4 +14,5 @@ print(lb, "lb is", "%0.5f"%getkg(lb), "kg")
 kg = int(input("input kg : "))
 print(lb, "kg is", "%0.5f"%getlb(kg), "lb")
 
+"""
 
