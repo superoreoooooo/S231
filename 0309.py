4a67f881d93b0c83ee75c7816f8ce7aca0c84a1f
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-03-09 11:29:37 +0900

    Update 0309.py

--- 0309.py
+++ 0309.py
@@ -16,3 +16,32 @@ print(lb, "kg is", "%0.5f"%getlb(kg), "lb")
 
 """
 
+"""
+
+total = 0
+
+for i in range (10) :
+    total -= 900
+
+for i in range (2) :
+    total += 1800
+
+for i in range (5) :
+    total -= 3500
+
+for i in range (4) :
+    total += 4000
+
+for i in range (1) :
+    total += 1500
+    
+for i in range (4) :
+    total += 2000
+    
+for i in range (5) :
+    total += 1800
+    
+print(total)
+
+"""
+
