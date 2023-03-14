60c214af79f61c128c96424b2a69903fc9a98e84
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-03-14 15:21:31 +0900

    Update 0314.py

--- 0314.py
+++ 0314.py
@@ -1,7 +1,15 @@
 import os
 
-score = int(input("~~"))
+py = 3
+mo = 2
+ex = 1
 
-print(score > 70)
+B = 3.5
+A0 = 4
+A = 4.5
+
+avg = (py * B + mo * A0 + ex * A) / (py + mo + ex)
+
+print("%.2f" % avg)
 
 os.system("pause")
