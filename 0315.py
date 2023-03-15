96cf97f41dd8fb2a41be5d94a612d91b1d5f06eb
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-03-15 15:28:36 +0900

    Update 0315.py

--- 0315.py
+++ 0315.py
@@ -1,6 +1,24 @@
 print("\"hello\"")
 
+"""
+
+*문자열 길이*
+
 str1 = len(input("1st str : "))
 str2 = len(input("2nd str : "))
 
 print("length diff : ", str1 - str2)
+"""
+
+"""
+
+*대/소문자*
+
+ss = "First Python"
+
+v1 = ss.upper()
+v2 = ss.lower()
+
+print(v1, "/", v2)
+"""
+
