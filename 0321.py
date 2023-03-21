35b15d6127e8c39afd1a0ea11536fafc7d8a4886
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-03-21 15:14:56 +0900

    Create 0321.py

---
+++ 0321.py
@@ -0,0 +1,41 @@
+import random
+
+"""
+
+age = int(input("age : "))
+
+if (age >= 18) :
+    print("pass")
+else :
+    print("fail")
+print("hello")
+"""
+
+status = ""
+
+pick = input("input : ")
+pc = random.choice(["rock", "paper", "scissors"])
+
+if (pick == pc) :
+    status = "draw"
+else :
+    if (pick == "rock") :
+        if (pc == "paper") :
+            status = "win"
+        else : 
+            status = "lose"
+    elif (pick == "paper") :
+        if (pc == "rock") :
+            status = "win"
+        else :
+            status = "lose"
+    elif (pick == "scissors") :
+        if (pc == paper) :
+            status = "win"
+        else :
+            status = "lose"
+    else :
+        status = "draw"
+
+print("pc : ", pc)
+print(status)   
