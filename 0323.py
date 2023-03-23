9d183a6ce1ec176be029de1f91362c9165539e1b
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-03-23 10:58:11 +0900

    Update 0323.py

--- 0323.py
+++ 0323.py
@@ -8,3 +8,17 @@ for i in range(2, 10, 1) :
 
 """
 
+sum = 0
+
+n1, n2 = 0, 0
+
+while True :
+    n1 = int(input("n1 : "))
+    if n1 == 0 :
+        break
+    n2 = int(input("n2 : "))
+    
+    sum = n1 + n2
+    print(num)
+    
+print("clear")
