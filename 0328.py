5e8248d8bdac5aee4e9d41c7007427c0dbb9166d
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-03-28 15:03:05 +0900

    Create 0328.py

---
+++ 0328.py
@@ -0,0 +1,60 @@
+import random
+"""
+
+*오늘의명언*
+
+wiseSay = [
+"삶이 있는 한 희망은 있다",
+"언제나 현재에 집중할 수 있다면 행복할 것이다",
+"신은 용기있는 자를 결코 버리지 않는다",
+"피할 수 없으면 즐겨라",
+"행복한 삶을 살기위해 필요한 것은 거의 없다",
+"내일은 내일의 태양이 뜬다",
+"계단을 밟아야 계단 위에 올라설 수 있다",
+"행복은 습관이다, 그것을 몸에 지니라",
+"1퍼센트의 가능성, 그것이 나의 길이다",
+"작은 기회로 부터 종종 위대한 업적이 시작된다"]
+
+print(wiseSay[random.randint(0, len(wiseSay) - 1)])
+
+"""
+
+l = list(range(0, 10))
+l[1] = 100
+
+sum = 0
+for i in range(0, len(l)) :
+    sum += l[i]
+
+print(sum)
+
+l[2] = 'not number'
+
+for i in range(10, 21) :
+    l.append(i)
+
+print(len(l))
+
+l2 = l.copy()
+l3 = l.copy()
+l4 = l.copy()
+l5 = l.copy()
+
+for i in range(0, 9) :
+    del(l2[0])
+
+for i in range(3, len(l3)) :
+    del(l3[3])
+    
+for i in range(0, len(l4)) :
+    if (type(l4[i]) is str) :
+        l4[i] = i
+
+for i in range(0, len(l5)) :
+    if (l5[i] >= 10) :
+        l5.remove(l5[i])
+
+print(l2)
+print(l3)
+print(l4)
+print(l5)
