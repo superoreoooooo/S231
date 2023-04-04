a89727b11b98c6d90823d089988a8916f2113245
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-04-04 15:03:46 +0900

    Create 0404.py

---
+++ 0404.py
@@ -0,0 +1,25 @@
+data = {}
+
+print("**** 물품과 재고량 입력 ****")
+
+while True:
+    k = input("입력 물품 : ")
+    
+    if (k == "") :
+        break
+    
+    v = int(input("재고량 : "))
+    
+    data[k] = v
+
+print("**** 물품과 재고량 확인 ****")
+
+while True:
+    k = input("찾을 물품 : ")
+    
+    if (k == "") :
+        break
+    elif (k in data.keys()) :
+        print(str(data[k]) + "개 남았어요.")
+    else :
+        print("그 물품은 없어요.")
