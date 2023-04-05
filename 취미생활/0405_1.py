e9ab91bc0954fa76c8fa7c6d3834345368fa94f3
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-04-05 09:54:34 +0900

    Update 0405_1.py

--- 취미생활/0405_1.py
+++ 취미생활/0405_1.py
@@ -1,3 +1,10 @@
 x = int(input("x : "))
 y = int(input("y : "))
 
+for dy in range(0, y, 1) :
+    for dx in range(dy, x + dy, 1) :
+        if (dx + 1 > x) :
+            print(dx + 1 - x, end = " ")
+        else :
+            print(dx + 1,  end = " ")
+    print("")
