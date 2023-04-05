6672a3f7155987e58f46f51a30a58f7d285d072e
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-04-05 13:35:44 +0900

    Create 박효기는바보다.java

---
+++ 취미생활/박효기는바보다.java
@@ -0,0 +1,40 @@
+package win.oreo.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        for (int dx = 0; dx < x; dx++) {
+            List<Integer> ls = new ArrayList<>();
+            for (int dy = dx; dy < dx + y; dy++) {
+                if (dy + 1 > y) {
+                    ls.add(dy + 1 - y);
+                }
+                else {
+                    ls.add(dy + 1);
+                }
+            }
+            list.add(dx, ls);
+        }
+
+        for (int i = 0; i < x; i++) {
+            List<Integer> ls = list.get(i);
+            for (int j = 0; j < y; j++) {
+                System.out.print(ls.get(j) + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
