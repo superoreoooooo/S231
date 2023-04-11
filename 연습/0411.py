36059bf74c9e7160169323bf1f2016293df6a082
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-04-11 15:10:28 +0900

    Update 0411.py

--- 연습/0411.py
+++ 연습/0411.py
@@ -1,1 +1,16 @@
 from datetime import datetime, timedelta
+
+def getCurrent() :
+    curDate = datetime.now()
+    return curDate
+
+def getAfter(now, day) :
+    afterDate = now + timedelta(days = day)
+    return afterDate
+
+curDate, afterDate = None, None
+curDate = getCurrent()
+
+afterDate = getAfter(curDate, 100)
+
+print("현재 날짜와 시간 =>", curDate, "\n100일 후 날짜와 시간 =>", afterDate)
