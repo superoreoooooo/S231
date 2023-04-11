d3f0c618fc900f2c7f1e52e6748b329ad9799e54
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-04-11 15:16:26 +0900

    Update 0411.py

--- 연습/0411.py
+++ 연습/0411.py
@@ -1,3 +1,5 @@
+"""
+
 from datetime import datetime, timedelta
 
 def getCurrent() :
@@ -14,3 +16,20 @@ curDate = getCurrent()
 afterDate = getAfter(curDate, 100)
 
 print("현재 날짜와 시간 =>", curDate, "\n100일 후 날짜와 시간 =>", afterDate)
+
+"""
+
+"""
+def isTrue(pwd) :
+    return pwd.isalnum()
+
+while (True) :
+    pwd = input("input password : ")
+    if (isTrue(pwd)) :
+        print("비밀번호가 올바르게 생성되었어요.")
+        break
+    else :
+        print("오류! 비밀번호가 규칙에 맞지 않습니다.")
+        
+"""
+
