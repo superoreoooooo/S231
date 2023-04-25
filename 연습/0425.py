5b83eb00b253c21c623f75450802384437d3e8a8
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-04-25 15:47:17 +0900

    "C:/Users/user/Documents/python/pytest/연습/write.txt"

--- 연습/0425.py
+++ 연습/0425.py
@@ -23,6 +23,31 @@ ff.close()
 
 """
 
+def copy(fn1, fn2) :
+    f = open(fn1, "r", encoding="UTF-8")
+    ff = open(fn2, "w", encoding="UTF-8")
+
+    fList = list()
+    f2 = f.readlines()
+
+    for i in f2 :
+        if (i.count("\n") >= 1) :
+            fList.append(i.split("\n")[0])
+        else :
+            fList.append(i)
+
+    for i in range(0, len(fList), 1) :
+        ff.writelines(fList[i] + "\n")
+
+    ff.writelines("\n=END=")
+
+    f.close()
+    ff.close()
+    
+    print("복제 완료~!")
+    
+copy("C:/Users/user/Documents/python/pytest/연습/kia.txt", "C:/Users/user/Documents/python/pytest/연습/write.txt")
+
 f = open("C:/Users/user/Documents/python/pytest/연습/keyboard.txt", "w", encoding="UTF-8")
 
 while True :
