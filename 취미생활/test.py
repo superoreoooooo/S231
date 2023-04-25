5c190c2fabde8b8796f8a87ac527cce7f60129e8
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-04-25 16:09:10 +0900

    ㅋ

--- 취미생활/test.py
+++ 취미생활/test.py
@@ -1,3 +1,21 @@
+import os
+
 f = open("C:/Users/user/Documents/python/pytest/취미생활/test.py", "r", encoding = "UTF-8")
 
 print(f"\n\n{f.read()}\n\n")
+
+for i in range(0, 10, 1) :
+    fList = list()
+    f2 = open("C:/Users/user/Documents/python/pytest/취미생활/test/test" + str(i) + ".py", "w", encoding = "UTF-8")
+
+    for i in f.readlines() :
+        if (i.count("\n") >= 1) :
+            fList.append(i.split("\n")[0])
+        else :
+            fList.append(i)
+
+    for i in range(0, len(fList), 1) :
+        f2.writelines(fList[i] + "\n")
+    f2.close()
+
+f.close()
