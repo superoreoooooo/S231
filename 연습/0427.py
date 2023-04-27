aa173acdb2f1ff19a1c610df3cb73beabc4993ec
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-04-27 11:25:50 +0900

    Create 0427.py

---
+++ 연습/0427.py
@@ -0,0 +1,135 @@
+"""
+class data:
+    __a = 10
+    b = "b"
+    
+    def __init__(self) :
+        self.__a = 20
+    
+    def method(self) :
+        return self.__a
+    
+d = data()
+
+print(d.method())
+
+"""
+
+"""
+class Car:
+    maxSpeed = 0
+    price = 0
+    __engine = ""
+    carName = ""
+    
+    def __init__(self, p, name) :
+        price = p
+        carName = name
+        print("============")
+        print("car price :", price, "car name :", carName)
+    
+    def goFoward(self) :
+        print("going forward", self.maxSpeed)
+    
+    def goBackward(self) :
+        print("going backward", int(self.maxSpeed / 2))
+        
+    def stop(self) :
+        print("stop")
+        
+    def startMusic(self, name) :
+        print("playing music :", name)
+        
+    def getEngine(self) :
+        return self.__engine
+        
+    def setEngine(self, name) :
+        self.__engine = name
+    
+    def __del__(self) :
+        print("car Crashed!")
+        print("============")
+        
+#car1 = Car(int(input("Car price : ")), input("Car name : "))
+car1 = Car(100, "corvette")
+
+car1.maxSpeed = 10
+
+car1.goFoward()
+car1.goBackward()
+car1.stop()
+car1.startMusic("highway to hell")
+car1.setEngine("GM 7.0 Liter V8 LS7 Engine")
+
+print(car1.getEngine())
+
+del(car1)
+"""
+
+class Vehicle:
+    __maxSpeed = 0
+    __name = ""
+    VehicleID = ""
+    
+    def __init__(self, ID) :
+        self.VehicleID = ID
+    
+    def goForward(self) :
+        print(self.__name, "is going forward :", self.__maxSpeed)
+    
+    def goBackward(self) :
+        print(self.__name, "going backward :", self.__maxSpeed / 2)
+    
+    def stop(self) :
+        print("stop!")
+    
+    def getMaxSpeed(self) :
+        return self.__maxSpeed
+    
+    def getName(self) :
+        return self.__name
+    
+    def setMaxSpeed(self, speed) :
+        self.__maxSpeed = speed
+    
+    def setName(self, name) :
+        self.__name = name
+    
+    def __del__(self) :
+        print(self.__name, "CRASH")
+
+class Car(Vehicle) :
+    engine = ""
+    
+    def __init__(self, ID, value) :
+        print("=====================")
+        self.VehicleID = ID
+        self.engine = value
+
+car1 = Car("AABB11", "V8")
+
+car1.setMaxSpeed(100)
+car1.setName("Corvette")
+
+car1.goForward()
+car1.goBackward()
+
+print(car1.getName(), car1.engine)
+
+class Bike(Vehicle) :
+    wheelCnt = 0
+    
+    def __init__(self, ID) :
+        print("=====================")
+        self.VehicleID = ID
+        self.wheelCnt = 2
+
+bike1 = Bike("BBCC22")
+
+bike1.setMaxSpeed(80)
+bike1.setName("kawasaki")
+
+bike1.goForward()
+bike1.goBackward()
+
+print(bike1.getName(), bike1.wheelCnt)
