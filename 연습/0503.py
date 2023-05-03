af17e96b2b0e160dab434121b9cdce37e2c59f0d
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-05-03 15:48:02 +0900

    Update 0503.py

--- 연습/0503.py
+++ 연습/0503.py
@@ -6,6 +6,11 @@ root = Tk()
 cnt = 0
 chk = IntVar()
 
+def reset() :
+    global cnt
+    cnt = 0
+    l2.configure(text = cnt)
+
 def fun() :
     global cnt
     cnt += 1
@@ -29,6 +34,9 @@ l2.pack()
 
 b1 = Button(root, text = "버튼", command = fun)
 b1.pack()
+
+b2 = Button(root, text = "Reset", command = reset)
+b2.pack()
 
 cb1 = Checkbutton(root, text = "테스트", variable=chk, command=fun2)
 cb1.pack()
