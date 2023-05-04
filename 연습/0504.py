09553477eda4afd8d7fcf10bd6f407efaa072954
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-05-04 11:07:29 +0900

    Update 0504.py

--- 연습/0504.py
+++ 연습/0504.py
@@ -6,14 +6,20 @@ root.title("Draw")
 root.geometry("1024x768")
 root.resizable(width = False , height = False)
 
-canv = Canvas(root, width=1024, height=768)
-canv.pack()
-
 def drawCircle(event) :
     canv.create_oval(event.x - 50, event.y - 50, event.x + 50, event.y + 50, width = 5, outline = "green")
 
 def drawRectangle(event) :
     canv.create_rectangle(event.x - 50, event.y - 50, event.x + 50, event.y + 50, width = 5, outline = "red")
+
+def clear() :
+    canv.delete("all")
+
+btn = Button(root, text = "CLEAR", command = clear)
+btn.pack(side = TOP)
+
+canv = Canvas(root, width=1024, height=768)
+canv.pack()
 
 root.bind("<Button-1>", drawCircle)
 root.bind("<Button-3>", drawRectangle)
