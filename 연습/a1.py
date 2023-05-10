6ac10ca2ba8cf3bad0a5cd8d0654329512e70002
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-05-10 15:12:10 +0900

    dasf

--- 연습/a1.py
+++ 연습/a1.py
@@ -4,9 +4,15 @@ class Ms:
     math = 0
     avg = 0
     
-    def __init__(self, kor, eng, math) :
-        self.avg = (kor + eng + math) / 3
+    def __init__(self, kor = 0, eng = 0, math = 0) :
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+    
+    def getAvg(self) :
+        self.avg = (self.kor + self.eng + self.math) / 3
     
 s1 = Ms(50, 60 ,70)
+s1.getAvg()
 
 print(s1.avg)
---
+++ 연습/a2.py
@@ -0,0 +1,6 @@
+from a1 import Ms
+
+s = Ms(40, 60, 80)
+s.getAvg()
+
+print(s.avg)
