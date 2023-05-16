625d1da0398d17bea97151662a3bbb1ef210e518
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-05-16 16:08:28 +0900

    dzgc

--- 연습/p0516.py
+++ 연습/p0516.py
@@ -1,11 +1,19 @@
 import os
+import random
 from PIL import Image, ImageFilter
 
-img = Image.open("C:/Users/user/Documents/python/source/photo/picture20.jpg")
+num = str(random.randint(1, 99))
+
+if (int(num) < 10) :
+    num = "0" + num
+
+img = Image.open("C:/Users/user/Documents/python/source/photo/picture" + num + ".jpg")
 
 img = img.transpose(Image.FLIP_TOP_BOTTOM)
 img = img.rotate(45, expand = True)
 img = img.filter(ImageFilter.CONTOUR())
 
 img.show()
-img.save("연습/photo/output.jpg")
+img.save("연습/photo/output" + num + ".jpg")
+
+print("File saved : output" + num + ".jpg")
